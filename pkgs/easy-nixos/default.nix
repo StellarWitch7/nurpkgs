fc85ffe0b2585e8f3fe1ed7dd09f15b897cc021c
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-04-04 23:41:03 -0400

    Update default.nix

--- pkgs/easy-nixos/default.nix
+++ pkgs/easy-nixos/default.nix
@@ -15,6 +15,7 @@ stdenv.mkDerivation rec {
     mkdir -p $out
     cp -r bin $out/bin
     chmod -R +x $out/bin
+    ln -s ${pkgs.home-manager.out}/bin/home-manager $out/bin/home-manager
   '';
 
   runtimeDeps = with pkgs; [
