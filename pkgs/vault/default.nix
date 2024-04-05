902aaeb418d482ed29c33c1ed3e7c349f44ca02f
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-04-05 17:31:09 -0400

    Updated vault to 1.1

--- pkgs/vault/default.nix
+++ pkgs/vault/default.nix
@@ -2,27 +2,31 @@
 
 stdenv.mkDerivation rec {
   name = "vault-${version}";
-  version = "1.0";
+  version = "1.1";
 
   src = fetchFromGitHub {
     owner = "StellarWitch7";
     repo = "vault";
     rev = "v${version}";
-    sha256 = "sha256-rYmVawkdVOBFuCzGN/pLpesXIQqADu6/Aw4EJhiCWRI=";
+    sha256 = "sha256-tvc++eezpC2EM1uGuQ3Za+GgKFe9xPhX4VBLWQyibxY=";
   };
 
   installPhase = ''
     mkdir -p $out
     cp -r bin $out/bin
     chmod -R +x $out/bin
+    ln -s ${pkgs.keepassxc.out}/bin/keepassxc-cli $out/bin/keepassxc-cli
     ln -s ${pkgs.sirikali.out}/bin/sirikali $out/bin/sirikali
   '';
 
-  runtimeDeps = [ pkgs.sirikali ];
+  runtimeDeps = with pkgs; [
+    keepassxc
+    sirikali
+  ];
 
   meta = with lib; {
     homepage = "https://github.com/StellarWitch7/vault";
-    description = "SiriKali wrapper scripts";
+    description = "KeePassXC-CLI and SiriKali wrapper scripts";
     license = licenses.mit;
   };
 }
