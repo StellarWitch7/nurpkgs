31742ed581f67cbe3f7f371215854a6b4a8c5198
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-09-18 12:45:58 -0400

    Update default.nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,4 @@
 {
   # Add your overlays here
-  #
-  # my-overlay = import ./my-overlay;
+  rust-overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/711ff40be18b09bf096930f52e1fb2fcbee9adf0.tar.gz");
 }
