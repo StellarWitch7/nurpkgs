05299f0c53c80c2fe9820190f2a66997b0c14cbb
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-10-27 22:57:22 -0400

    It has been patched

--- pkgs/dont-repeat-yourself/default.nix
+++ pkgs/dont-repeat-yourself/default.nix
@@ -1,22 +1,48 @@
 { lib
 , fetchFromGitHub
-, rustPlatform }:
+, rustPlatform
+, makeWrapper
+, xorg
+, libxkbcommon
+, libGL }:
 
-rustPlatform.buildRustPackage rec {
+let
+  maintainer = "StellarWitch7";
+in rustPlatform.buildRustPackage rec {
   pname = "dont-repeat-yourself";
   version = "1.0.0";
 
   src = fetchFromGitHub {
-    owner = "StellarWitch7";
+    owner = maintainer;
     repo = pname;
     rev = "v${version}";
     sha256 = "sha256-XSsVctUzJNV0B/X3eWH4ONpRlzFzr03UBjr42H1srO8=";
   };
 
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
+  buildInputs = [
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libxcb
+    xorg.libXi
+    libxkbcommon
+    libGL
+  ];
+
+  postInstall = ''
+    wrapProgram $out/bin/${pname} --set LD_LIBRARY_PATH ${lib.makeLibraryPath buildInputs}
+  '';
+
   cargoLock.lockFile = "${src}/Cargo.lock";
   doCheck = false;
 
   meta = with lib; {
+    homepage = "https://github.com/${maintainer}/${pname}";
+    description = "Keyboard-only clipboard manager";
     license = licenses.mit;
+    maintainers = [ maintainer ];
   };
 }
