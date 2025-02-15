79be4e07174487fd48d70deb9c1ecabf5c9da9bb
Aurora Dawn <aurora.dawn@trans.fish>
2025-02-15 12:36:32 -0500

    Yippee

--- pkgs/clickr/default.nix
+++ pkgs/clickr/default.nix
@@ -4,13 +4,13 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "clickr";
-  version = "2.0.0";
+  version = "2.0.1";
 
   src = fetchFromGitHub {
     owner = "StellarWitch7";
     repo = "clickr";
     rev = "v${version}";
-    sha256 = "sha256-eWMsBkXvDQBmKN5Vx6+1EYEnT95vJEkBGL5TTfrGyWE=";
+    sha256 = "sha256-Ysld8ec9Xphd8BWKrtF2lRrXv56O99s/pztqHuWEJIE=";
   };
 
   buildInputs = [
